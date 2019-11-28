99968a8d950a4d3ddf534ba8c451f9566b523b5f
Marcos Tirao <>
2019-11-28 14:56:16 -0300

    Added new endpoint

--- src/Fields.hs
+++ src/Fields.hs
@@ -26,6 +26,16 @@ instance ToJSON Fields where
         ,  "next" .= next
         ]
 
+data FieldType = FieldType 
+    { sport :: Text
+    , players :: [Int]
+    }
+
+instance ToJSON FieldType where
+    toJSON FieldType {..} = object
+        [ "type" .= sport
+        , "players" .= players
+        ]
 
 data Field = Field
     { name :: Text
@@ -34,8 +44,8 @@ data Field = Field
     , phone :: Text
     , cell_phone :: Text
     , url :: Text
-    , amenities :: Text
-    , field_type :: Text
+    , amenities :: [Text]
+    , field_type :: FieldType
     , score :: Int
     , id :: Int
     }
@@ -54,13 +64,17 @@ instance ToJSON Field  where
         , "id" .= id
         ]
 
-field_1 = Field "El Protrero Soccer 1" "Belgrano, CABA." "Av. Cabildo 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 1 1
-field_2 = Field "El Protrero Soccer 2" "Belgrano, CABA." "Av. Libertador 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 2 2
-field_3 = Field "El Protrero Soccer 3" "Devoto, CABA." "Av. Beiro 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 3 3
-field_4 = Field "El Protrero Soccer 4" "Monte Castro, CABA." "Av. Alvarez Jonte 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 3 4
-field_5 = Field "El Protrero Soccer 5" "Almagro, CABA." "Av. San Martin 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 4 5
-field_6 = Field "El Protrero Soccer 6" "Belgrano, CABA." "Av. Chivilcoy 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 5 6
-field_7 = Field "El Protrero Soccer 7" "Belgrano, CABA." "Av. Belgrano 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" "Cesped Sintetico - Techada - Iluminicion" "Futbol 7 - 9 - 11" 1 7
+sport_1 =  FieldType "Soccer" [5,6,7,8,9,10]
+
+amenities_1 = ["Cesped", "Sintetico", "Techada",  "Iluminicion"]
+
+field_1 = Field "El Protrero Soccer 1" "Belgrano, CABA." "Av. Cabildo 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 1 1
+field_2 = Field "El Protrero Soccer 2" "Belgrano, CABA." "Av. Libertador 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 2 2
+field_3 = Field "El Protrero Soccer 3" "Devoto, CABA." "Av. Beiro 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 3 3
+field_4 = Field "El Protrero Soccer 4" "Monte Castro, CABA." "Av. Alvarez Jonte 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 3 4
+field_5 = Field "El Protrero Soccer 5" "Almagro, CABA." "Av. San Martin 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 4 5
+field_6 = Field "El Protrero Soccer 6" "Belgrano, CABA." "Av. Chivilcoy 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 5 6
+field_7 = Field "El Protrero Soccer 7" "Belgrano, CABA." "Av. Belgrano 4000" "11 1234-5678" "11 1234-34567" "www.someurl.com" amenities_1 sport_1 0 7
 
 
 
