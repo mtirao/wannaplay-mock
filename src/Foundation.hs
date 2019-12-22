794ee9528d4e86e5c64e18de413c306ab7246ef5
Marcos Tirao <mtirao@gmail.com>
2019-12-22 19:58:56 -0300

    Added SQLLite support

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -5,9 +5,31 @@
 module Foundation where
 
 import Yesod.Core
+import Yesod
+import Database.Persist.Sqlite
+import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Logger (runStderrLoggingT)
 
 data App = App
 
 mkYesodData "App" $(parseRoutesFile "routes")
 
 instance Yesod App
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person
+    firstName String
+    lastName String
+    age Int
+    deriving Show
+|]
+
+data PersistTest = PersistTest ConnectionPool
+
+instance YesodPersist PersistTest where
+    type YesodPersistBackend PersistTest = SqlBackend
+
+    runDB action = do
+        PersistTest pool <- getYesod
+        runSqlPool action pool
+
