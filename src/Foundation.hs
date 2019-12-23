5dc432990febed8b43a7bff24a0b5dd064b006fc
Marcos Tirao <>
2019-12-22 22:38:57 -0300

    Add model parsing from file.

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE ViewPatterns      #-}
 module Foundation where
 
+import ClassyPrelude.Yesod
+import Control.Monad.IO.Class  (liftIO)
 import Yesod.Core
 import Yesod
+import Database.Persist
+import Database.Persist.TH
 import Database.Persist.Sqlite
 import Control.Monad.Trans.Resource (runResourceT)
 import Control.Monad.Logger (runStderrLoggingT)
+import Database.Persist.Quasi
+import Data.Text (Text)
 
 data App = App
 
@@ -16,13 +27,9 @@ mkYesodData "App" $(parseRoutesFile "routes")
 
 instance Yesod App
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
-    firstName String
-    lastName String
-    age Int
-    deriving Show
-|]
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "config/models")
+
 
 data PersistTest = PersistTest ConnectionPool
 
